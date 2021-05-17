968001c1a2f19a3933e444854e0f9162fdbc4f67
joel-kurian <83607509+joel-kurian@users.noreply.github.com>
2021-05-17 15:03:00 -0400

    Add files via upload

---
+++ SpringExamples/AutowiredExample/src/com/capgemini/Employee.java
@@ -0,0 +1,29 @@
+package com.capgemini;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Employee{
+	private int empID;
+	private String empName;
+	@Autowired
+	private Address address;
+	
+	public int getEmpID() {
+		return empID;
+	}
+	public void setEmpID(int empID) {
+		this.empID = empID;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}
