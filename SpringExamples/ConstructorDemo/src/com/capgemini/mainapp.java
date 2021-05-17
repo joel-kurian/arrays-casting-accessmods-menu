2fde1e79076afa1280738dc4df92800b3231bb44
joel-kurian <83607509+joel-kurian@users.noreply.github.com>
2021-05-17 15:07:25 -0400

    Add files via upload

---
+++ SpringExamples/ConstructorDemo/src/com/capgemini/mainapp.java
@@ -0,0 +1,17 @@
+package com.capgemini;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class mainapp {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		//context.registerShutdownHook();
+		Employee emp = (Employee) context.getBean("emp1");
+        
+		System.out.println(emp.getEmpID());
+		System.out.println(emp.getEmpName());
+		//System.out.println(emp.getAddresses().toString());
+	}
+}
